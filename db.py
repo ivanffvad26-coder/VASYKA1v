8618e352a1384fa98c090fc5847c4f3c611b262b
VASYKA <ivanffvad26@gmail.com>
2026-01-20 19:09:35 +0300

    Fix sqlite url

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "sqlite://db.sqlite3"
+DATABASE_URL = "sqlite:///db.sqlite3"
 
 engine = create_engine(
     DATABASE_URL,
