fc846b67e997665bcf096412b77b985c3a21cd55
VASYKA <ivanffvad26@gmail.com>
2026-01-20 18:16:22 +0300

    Fix SQLite database URL

--- db.py
+++ db.py
@@ -1,15 +1,16 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite://db.sqlite3"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///db.sqlite3"
+)
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
