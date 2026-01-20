d48f7bf2769a688a159c4f9c299dd14d23e426a6
VASYKA <ivanffvad26@gmail.com>
2026-01-20 17:48:11 +0300

    Fix Twilio sms integration

--- sms.py
+++ sms.py
@@ -1,15 +1,16 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ.get("TWILLIO_SID")
-auth_token = os.environ.get("TWILIO_TOKEN")
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 twilio_phone = os.environ.get("TWILIO_PHONE")
 
 client = Client(account_sid, auth_token)
 
 def send_sms(phone, code):
-    client.messages.create(
+    message = client.messages.create(
         body=f"Ваш код подтверждения: {code}",
-        from=twilio_phone,
+        from_=twilio_phone,
         to=phone
     )
+    return message.sid
