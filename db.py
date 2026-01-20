326f7cd619d1cc169561bfe279bead39c2bdaaa6
VASYKA <ivanffvad26@gmail.com>
2026-01-20 18:49:59 +0300

    Fix db

--- db.py
+++ db.py
@@ -2,11 +2,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+print("DATABASE_URL ENV =", os.getenv("DATABASE_URL"))
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "sqlite:///db.sqlite3"
 )
 
+print("DATABASE_URL USED=", DATABASE_URL)
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
