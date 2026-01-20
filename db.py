98644c4b7627120edbffe070d25763acf6bb37a9
VASYKA <ivanffvad26@gmail.com>
2026-01-20 22:20:19 +0300

    remove PhoneCode model completely

--- db.py
+++ db.py
@@ -2,16 +2,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+        return db
     finally:
         db.close()
--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from db import Base
 
-
 class User(Base):
-    __tablename__ = "users"
+    tablename = "users"
 
     id = Column(Integer, primary_key=True)
     phone = Column(String, unique=True, nullable=False)
