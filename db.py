0401e7722668d5a2f60a9928fb98874a5403d98e
VASYKA <ivanffvad26@gmail.com>
2026-01-20 19:02:05 +0300

    Fix db 3

--- db.py
+++ db.py
@@ -1,18 +1,15 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
-print("DATABASE_URL ENV =", os.getenv("DATABASE_URL"))
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///db.sqlite3"
-)
+DATABASE_URL = "sqlite://db.sqlite3"
 
-print("DATABASE_URL USED=", DATABASE_URL)
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
