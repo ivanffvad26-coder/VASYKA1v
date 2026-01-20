b8b5911c1c731199a9133b173b024addbe7f8f13
VASYKA <ivanffvad26@gmail.com>
2026-01-20 22:28:44 +0300

    fix sqlalchemy models

--- models.py
+++ models.py
@@ -1,8 +1,19 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
 from db import Base
+
 
 class User(Base):
     tablename = "users"
 
     id = Column(Integer, primary_key=True)
     phone = Column(String, unique=True, nullable=False)
+
+
+class PhoneCode(Base):
+    tablename = "phone_codes"   # ← ВОТ ЭТОГО У ТЕБЯ НЕ БЫЛО
+
+    id = Column(Integer, primary_key=True)
+    phone = Column(String, nullable=False)
+    code = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
