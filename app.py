60602f8a90e142be2715df7e4b46b6260e5c4997
VASYKA <ivanffvad26@gmail.com>
2026-01-20 20:02:30 +0300

    Remove engine usage from app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 print("=== ЭТО МОЙ APP.PY ===")
 
 from flask import Flask, render_template, request, redirect, url_for, session
-import json
-import os
-from sqlalchemy import create_engine, text
-
 from auth import auth
+from json import json
+from db import init_db
 
 app = Flask(__name__)
 app.secret_key = "secret-key"
 
 app.register_blueprint(auth)
+
+init_db()
 
 @app.route("/")
 def home():
@@ -19,17 +19,6 @@ def home():
 
 if __name__ == "__main__":
     app.run()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-
-try:
-    with engine.connect() as conn:
-        conn.execute(text("SELECT 1"))
-        print(" DATABASE CONNECTED")
-except Exception as e:
-    print("DATABASE ERROR:", e)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 USERS_FILE = os.path.join(BASE_DIR, "users.json")
--- db.py
+++ db.py
@@ -3,20 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"sqlite:///{DB_PATH}"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
-    return SessionLocal()
+    db = SessionLocal()
+    try:
+        return db
+    finally:
+        pass
 
 def init_db():
     Base.metadata.create_all(bind=engine)
